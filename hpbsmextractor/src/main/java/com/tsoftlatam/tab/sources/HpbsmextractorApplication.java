634727ff18e3996e8476584f9f5f0066ea5d5da2
david.malagueno <david.malagueno@tsoftlatam.com>
2017-05-02 17:12:18 -0300

    Modificación Modulo Extractor del WebService

--- hpbsmextractor/src/main/java/com/tsoftlatam/tab/sources/HpbsmextractorApplication.java
+++ hpbsmextractor/src/main/java/com/tsoftlatam/tab/sources/HpbsmextractorApplication.java
@@ -3,7 +3,10 @@ package com.tsoftlatam.tab.sources;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
+//Para que reconozca al ExtractorController que está en un package diferente
+@ComponentScan(basePackages = {"com.tsoftlatam.tab.controllers"})
 @EnableDiscoveryClient
 @SpringBootApplication
 public class HpbsmextractorApplication {
