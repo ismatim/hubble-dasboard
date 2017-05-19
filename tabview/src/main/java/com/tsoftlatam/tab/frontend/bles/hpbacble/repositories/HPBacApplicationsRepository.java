edfbcfb87d3d95e49fbdb5fe83191dbcab534130
Matías Lapalma <matias.lapalma@tsoftlatam.com>
2017-05-19 16:19:47 -0300

    Eliminación de todo

--- tabview/src/main/java/com/tsoftlatam/tab/frontend/bles/hpbacble/repositories/HPBacApplicationsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tsoftlatam.tab.frontend.bles.hpbacble.repositories;
-
-import com.tsoftlatam.tab.frontend.bles.hpbacble.models.HPBacApplication;
-import org.springframework.data.repository.CrudRepository;
-
-public interface HPBacApplicationsRepository extends CrudRepository<HPBacApplication, Integer> {
-}
--- tabview/src/main/java/com/tsoftlatam/tab/frontend/bles/hpbacble/repositories/HPBacTransactionsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tsoftlatam.tab.frontend.bles.hpbacble.repositories;
-
-
-import com.tsoftlatam.tab.frontend.bles.hpbacble.models.HPBacTransaction;
-import org.springframework.data.repository.CrudRepository;
-
-public interface HPBacTransactionsRepository extends CrudRepository<HPBacTransaction, Integer> {
-}
