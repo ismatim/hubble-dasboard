9fbb858f5cadd95d13d81cd7240c9e3c4a5b09ac
Ismael J. Tisminetzky <ismatim@gmail.com>
2017-07-20 12:28:45 -0300

    removing core.utils

--- hubble.backend.tasksrunner/src/test/java/hubble/backend/tasksrunner/tests/providerjob/AppPulseTaskUnitTests.java
+++ hubble.backend.tasksrunner/src/test/java/hubble/backend/tasksrunner/tests/providerjob/AppPulseTaskUnitTests.java
@@ -1,17 +1,11 @@
 package hubble.backend.tasksrunner.tests.providerjob;
+//@RunWith(MockitoJUnitRunner.class)
+//@ContextConfiguration(classes = TasksRunnerTestConfiguration.class)
 
-import hubble.backend.tasksrunner.tests.configurations.TasksRunnerTestConfiguration;
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-
-@ActiveProfiles("test")
-@RunWith(MockitoJUnitRunner.class)
-@ContextConfiguration(classes = TasksRunnerTestConfiguration.class)
 public class AppPulseTaskUnitTests {
 
     public AppPulseTaskUnitTests() {
 
     }
+
 }
