49eb70c6b5cf0e501bfa11999082deae8bba12b1
Ismael J. Tisminetzky <ismatim@gmail.com>
2017-10-18 14:49:51 +0000

    Bug de ALM - Fechas Incorrectas

--- hubble.backend.providers/src/main/java/hubble/backend/providers/configurations/mappers/alm/AlmDefectToIssueConverter.java
+++ hubble.backend.providers/src/main/java/hubble/backend/providers/configurations/mappers/alm/AlmDefectToIssueConverter.java
@@ -9,19 +9,19 @@ import org.modelmapper.AbstractConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class AlmDefectToIssueConverter extends AbstractConverter<AlmDefectProviderModel,IssueStorage> {
-   
+public class AlmDefectToIssueConverter extends AbstractConverter<AlmDefectProviderModel, IssueStorage> {
+
     private final Logger logger = LoggerFactory.getLogger(AlmDefectToIssueConverter.class);
-    
+
     @Override
     protected IssueStorage convert(AlmDefectProviderModel s) {
         IssueStorage issue = new IssueStorage();
-        issue.setClosedDate(stringToDate(s.getClosedDate(),"dd-MM-yyyy"));
-        issue.setModifiedDate(stringToDate(s.getModifiedDate(),"dd-MM-yyyy HH:mm:ss"));
+        issue.setClosedDate(stringToDate(s.getClosedDate(), "yyyy-MM-dd"));
+        issue.setModifiedDate(stringToDate(s.getModifiedDate(), "yyyy-MM-dd"));
         issue.setPriority(selectorToInteger(s.getPriority()));
-        issue.setRegisteredDate(stringToDate(s.getRegisteredDate(),"dd-MM-yyyy"));
+        issue.setRegisteredDate(stringToDate(s.getRegisteredDate(), "yyyy-MM-dd"));
         issue.setReproducible(reproducibleToBoolean(s.getReproducible()));
-        issue.setSeverity(selectorToInteger(s.getSeverity()));  
+        issue.setSeverity(selectorToInteger(s.getSeverity()));
         issue.setBusinessApplicationId(s.getApplicationId());
         issue.setExternalId(s.getId());
         issue.setAssignee(s.getAssignee());
@@ -32,32 +32,35 @@ public class AlmDefectToIssueConverter extends AbstractConverter<AlmDefectProvid
         issue.setDetectedOnRelease(s.getDetectedOnRelease());
         issue.setExternalId(s.getId());
         issue.setProviderName(s.getProviderName());
-        issue.setProviderOrigin(s.getProviderOrigin());        
+        issue.setProviderOrigin(s.getProviderOrigin());
         issue.setStatus(s.getStatus());
         issue.setTitle(s.getTitle());
         issue.setTransaction(s.getTransaction());
-        
+
         return issue;
     }
-    
-    private Boolean reproducibleToBoolean(String isReproducible){
-        if(isReproducible==null)
+
+    private Boolean reproducibleToBoolean(String isReproducible) {
+        if (isReproducible == null) {
             return false;
+        }
         return isReproducible.equals("Y");
     }
-    
-    private int selectorToInteger(String comboBoxSelectedValue){
-        if(!isValid(comboBoxSelectedValue))
+
+    private int selectorToInteger(String comboBoxSelectedValue) {
+        if (!isValid(comboBoxSelectedValue)) {
             return 0;
+        }
         String subString = comboBoxSelectedValue.split("-")[0].split(" ")[0];
         return Integer.valueOf(subString);
     }
-    
-    private Date stringToDate(String dateFormatString, String format){
-        if(!isValid(dateFormatString))
-           return null;
+
+    private Date stringToDate(String dateFormatString, String format) {
+        if (!isValid(dateFormatString)) {
+            return null;
+        }
         SimpleDateFormat formatter = new SimpleDateFormat(format);
-        Date dateToReturn=new Date();        
+        Date dateToReturn = new Date();
         try {
             dateToReturn = formatter.parse(dateFormatString);
         } catch (ParseException ex) {
@@ -65,11 +68,12 @@ public class AlmDefectToIssueConverter extends AbstractConverter<AlmDefectProvid
         }
         return dateToReturn;
     }
-    
-    private boolean isValid(String stringToValidate){
-        if(stringToValidate==null || stringToValidate.equals(""))
+
+    private boolean isValid(String stringToValidate) {
+        if (stringToValidate == null || stringToValidate.equals("")) {
             return false;
+        }
         return true;
     }
-     
+
 }
