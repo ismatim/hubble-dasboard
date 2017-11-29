f08268c78e85cbb158ee9081878dec8ef8030872
admin <andre.vigneaux@tsoftlatam.com>
2017-11-29 12:30:40 -0300

    Se agrega clase mapper configuration

---
+++ hubble.backend.providers/src/main/java/hubble/backend/providers/configurations/mappers/jira/JiraMapperConfiguration.java
@@ -0,0 +1,16 @@
+package hubble.backend.providers.configurations.mappers.jira;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JiraMapperConfiguration {
+
+	private ModelMapper mapper;
+	
+	public JiraMapperConfiguration() {
+		this.mapper = new ModelMapper();
+		this.mapper.addMappings(new IssuePropertyMap());
+		this.mapper.addMappings(new ApplicationPropertyMap());
+	}
+}
